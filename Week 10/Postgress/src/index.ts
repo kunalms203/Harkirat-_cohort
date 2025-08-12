af9dead810a8fa7857eff1eaca6de6b3f98d380b
kunalms203 <ksreviews8@gmail.com>
2025-08-12 23:24:46 +0530

    learned pg

---
+++ Week 10/Postgress/src/index.ts
@@ -0,0 +1,32 @@
+// db.ts
+import { Client } from "pg";
+
+const client = new Client({
+  connectionString: "postgresql://neondb_owner:npg_NS7C2UlhJiGu@ep-lingering-heart-a1aqqnm3-pooler.ap-southeast-1.aws.neon.tech/kunal?sslmode=require&channel_binding=require",
+  ssl: { rejectUnauthorized: false },
+});
+
+const createUsersTable = async () => {
+  try {
+    await client.connect();
+    console.log("Connected to db");
+
+    // Create table directly in your connected database
+    const result = await client.query(`
+        
+      CREATE TABLE IF NOT EXISTS users (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(20),
+        age INT
+      )
+    `);
+
+    console.log("Table created:", result.command);
+  } catch (err) {
+    console.error("Error:", err);
+  } finally {
+    await client.end();
+  }
+};
+
+createUsersTable();
